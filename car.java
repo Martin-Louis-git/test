e414f1a530cdea1750b5a09e18b67eee80cb5291
Ahmed-Ramy <ahmedramy285@gmail.com>
2025-02-28 21:26:26 +0200

    see me?

--- car.java
+++ car.java
@@ -1,10 +1,11 @@
 public class car {
     int x;
-    String b="fuck guys";
+   
     public car(){
 
     }
     public static void main(String[] args){
-        System.out.print(b);
+       
+        System.out.println("fuck tarek");
     }
 }
